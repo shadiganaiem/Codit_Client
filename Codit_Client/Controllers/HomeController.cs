b6a48c94f310751d95aff1e0bd6bf03c24171b22
Shadi Ghnaiem <Shadi.stu@gmail.com>
2019-09-11 18:02:11 +0300

    changes

--- Codit_Client/Controllers/HomeController.cs
+++ Codit_Client/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Codit_Client.Models;
 namespace Codit_Client.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+
+
+            return View(new Client());
+        }
+
+       
+        [HttpPost]
+        public ActionResult SendInformation()
+        {
+
+
+            return View("Index",new Client());
         }
     }
 }
---
+++ Codit_Client/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Codit_Client.Models
+{
+    public class Client
+    {
+        [Required]
+        public string fullName { get; set; } = string.Empty;
+
+        [Required]
+        public string email { get; set; } = string.Empty;
+
+        [Required]
+        public string phone { get; set; } = string.Empty;
+    }
+}
